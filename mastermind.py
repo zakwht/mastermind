227ccfce1181fdeeb104d0777a1cf80a2c1b048a
Zak White <zakwht@gmail.com>
2018-10-20 21:38:44 -0700

    Fix Logic, Finish Game

--- mastermind.py
+++ mastermind.py
@@ -4,8 +4,10 @@ import random
 
 colour_array = ['PaleVioletRed1', 'aquamarine2', 'peach puff', 'DarkOliveGreen2', 'SlateBlue2']
 seq = ['white', 'white', 'white', 'white']
-theme = 0
-diff = 1
+theme = 0       #colour theme
+diff = 1        #difficulty [easy, standard, hard]
+destroy = False #flag to close popup & restart game
+kill = False    #flag to close all windows & exit
 
 ##########################
 # Selection Button Class #
@@ -16,15 +18,15 @@ class CustomButton(tk.Canvas):
         self.colour = colour_array[0]
         tk.Canvas.__init__(self, parent, borderwidth=0,
             relief="raised", highlightthickness=0)
-        id = self.create_oval((4,4,50,50), outline=self.colour, fill=self.colour)
+        id = self.create_oval((4,4,50,50), outline='black', fill=self.colour)
         self.configure(width=54, height=54)
         self.bind("<ButtonPress-1>", self.set_colour)
 
     def set_colour(self, event): #iterate through colours
         self.state += 1
-        self.state %= 5
+        self.state %= len(colour_array)
         self.colour = colour_array[self.state]
-        id = self.create_oval((4,4,50,50), outline=self.colour, fill=self.colour)
+        id = self.create_oval((4,4,50,50), outline='black', fill=self.colour)
 
 ######################
 # Enter Button Class #
@@ -82,10 +84,99 @@ class Peg(tk.Canvas):
         id = self.create_oval((5,5,10,10), outline='black', fill=colour)
         self.configure(width=10, height=10)
 
+# Menu for Selecting Colours
+def colour_menu():
+    global theme
+    pop = tk.Toplevel()
+    pop.wm_title("Colours")
+    pop.protocol("WM_DELETE_WINDOW", set_destroy)
+    v = tk.IntVar()
+    v.set(theme)
+
+    tk.Label(pop, text="Select Colour Theme:").pack()
+    tk.Radiobutton(pop, text="Default", padx = 10, variable=v, value=0).pack(anchor=tk.W)
+    tk.Radiobutton(pop, text="Greyscale", padx = 10, variable=v, value=1).pack(anchor=tk.W)
+    tk.Radiobutton(pop, text="Blue-green", padx = 10, variable=v, value=2).pack(anchor=tk.W)
+    tk.Radiobutton(pop, text="Halloween", padx = 10, variable=v, value=3).pack(anchor=tk.W)
+    pop.resizable(0, 0)
+
+    global destroy
+    destroy = False
+    while(not destroy):
+        pop.update()
+        theme = v.get()
+        set_colour(v.get())
+    pop.destroy()
+
+# Menu for Selecting Difficulty
+def difficulty_menu():
+    global diff
+    pop = tk.Toplevel()
+    pop.wm_title("Difficulty")
+    pop.protocol("WM_DELETE_WINDOW", set_destroy)
+    v = tk.IntVar()
+    v.set(diff)
+
+    tk.Label(pop, text="Select Colour Theme:").pack()
+    tk.Radiobutton(pop, text="Easy", padx = 10, variable=v, value=0).pack(anchor=tk.W)
+    tk.Radiobutton(pop, text="Standard", padx = 10, variable=v, value=1).pack(anchor=tk.W)
+    tk.Radiobutton(pop, text="Hard", padx = 10, variable=v, value=2).pack(anchor=tk.W)
+    pop.resizable(0, 0)
+
+    global destroy
+    destroy = False
+    while(not destroy):
+        pop.update()
+        theme = v.get()
+        set_diff(v.get())
+    pop.destroy()
+
+# Instructions Menu
+def instructions():
+    pop = tk.Toplevel()
+    pop.wm_title("Instructions")
+    pop.resizable(0, 0)
+    T = Text(pop, height=9, width=50, bd=0, wrap=tk.WORD)
+    T.pack(side=LEFT, fill=Y)
+    s = "How To Play\n"
+    s += "A random code of 5 colours is generated. You have twelve tries to"
+    s += "crack the code. Each turn, use the buttons to choose the colour "
+    s += "combination you want to guess. For each colour in the right spot, "
+    s += "you will receive a black peg (left side). For any other colour "
+    s += "you guessed in the wrong spot, you will receive one white peg "
+    s += "(right). If you find the code before 12 tries, you win!"
+    T.insert(END, s)
+    T.config(state=DISABLED)
+
+def set_destroy():
+    global destroy
+    destroy = True
+
+def set_kill():
+    global kill
+    kill = True
+
+# Helper Method for Changing Colours
+def set_colour(colour):
+    global colour_array
+    halloween = ['gray4', 'DarkOrange1', 'yellow', 'orange4', 'ghost white']
+    default = ['PaleVioletRed1', 'aquamarine2', 'peach puff', 'DarkOliveGreen2', 'SlateBlue2']
+    bluegreen = ['SeaGreen1', 'dodger blue', 'spring green', 'aquamarine', 'steel blue']
+    grey = ['gray90', 'gray60', 'gray30', 'gray20', 'gray5']
+    themes = [default, grey, bluegreen, halloween]
+    colour_array = themes[colour]
+
+# Helper Method for Changing Difficulty
+def set_diff(d):
+    global diff
+    diff = d
+
 # Create Random Colour Code
 def generate_sequence():
     global colour_array
     global seq
+    if diff == 0:
+        colour_array = colour_array[:-1]
     for i in range(4):
         seq[i] = random.choice(colour_array)
 
@@ -107,79 +198,41 @@ def check_pegs(colours):
             if i == j or bool2[j] == 't' or seq[i]!=colours[j]:
                 continue
             white += 1
+            bool2[j] = 't'
             break
     return [black, white]
 
-# Menu for Selecting Colours
-def colour_menu():
-    global theme
+# Popup for Victory
+def vic_pop(tries):
     pop = tk.Toplevel()
-    pop.wm_title("Colours")
-    v = tk.IntVar()
-    v.set(theme)
+    pop.wm_title("Congratulations!")
+    pop.protocol("WM_DELETE_WINDOW", set_destroy)
 
-    tk.Label(pop, text="Select Colour Theme:").pack()
-    tk.Radiobutton(pop, text="Default", padx = 10, variable=v, value=0).pack(anchor=tk.W)
-    tk.Radiobutton(pop, text="Greyscale", padx = 10, variable=v, value=1).pack(anchor=tk.W)
-    tk.Radiobutton(pop, text="Blue-green", padx = 10, variable=v, value=2).pack(anchor=tk.W)
-    tk.Radiobutton(pop, text="Halloween", padx = 10, variable=v, value=3).pack(anchor=tk.W)
-
-    while(True):
-        pop.update()
-        theme = v.get()
-        set_colour(v.get())
-
-# Helper Method for Changing Colours
-def set_colour(colour):
-    global colour_array
-    halloween = ['gray4', 'DarkOrange1', 'ghost white', 'orange3', 'yellow']
-    default = ['PaleVioletRed1', 'aquamarine2', 'peach puff', 'DarkOliveGreen2', 'SlateBlue2']
-    bluegreen = ['SeaGreen1', 'dodger blue', 'spring green', 'aquamarine', 'steel blue']
-    grey = ['gray90', 'gray60', 'gray30', 'gray20', 'gray5']
-    themes = [default, grey, bluegreen, halloween]
-    colour_array = themes[colour]
-
-# Menu for Selecting Difficulty
-def difficulty_menu():
-    global diff
-    pop = tk.Toplevel()
-    pop.wm_title("Difficulty")
-    v = tk.IntVar()
-    v.set(diff)
-
-    tk.Label(pop, text="Select Colour Theme:").pack()
-    tk.Radiobutton(pop, text="Easy", padx = 10, variable=v, value=0).pack(anchor=tk.W)
-    tk.Radiobutton(pop, text="Standard", padx = 10, variable=v, value=1).pack(anchor=tk.W)
-    tk.Radiobutton(pop, text="Hard", padx = 10, variable=v, value=2).pack(anchor=tk.W)
-    # (12,4):(12,5):(8,5)
-
-    while(True):
-        pop.update()
-        theme = v.get()
-        set_diff(v.get())
-
-# Helper Method for Changing Difficulty
-def set_diff(d):
-    global diff
-    diff = d
-
-# Instructions Menu
-def instructions():
-    pop = tk.Toplevel()
-    pop.wm_title("Instructions")
+    mv = " move!" if tries==1 else " moves"
+    s = "You cracked the code in " + str(tries) + mv
+    tk.Label(pop, text=s).pack()
+    tk.Button(pop, text="Play Again", padx = 10, command=set_destroy).pack()
+    tk.Button(pop, text="Exit", padx = 10, command=set_kill).pack()
     pop.resizable(0, 0)
-    T = Text(pop, height=9, width=50, bd=0, wrap=tk.WORD)
-    T.pack(side=LEFT, fill=Y)
-    s = "How To Play\n"
-    s += "A random code of 5 colours is generated. You have twelve tries to"
-    s += "crack the code. Each turn, use the buttons to choose the colour "
-    s += "combination you want to guess. For each colour in the right spot, "
-    s += "you will receive a black peg (left side). For any other colour "
-    s += "you guessed in the wrong spot, you will receive one white peg "
-    s += "(right). If you find the code before 12 tries, you win!"
-    T.insert(END, s)
-    T.config(state=DISABLED)
 
+# Popup for loss
+def loss_pop():
+    pop = tk.Toplevel()
+    pop.wm_title("Sorry!")
+    pop.protocol("WM_DELETE_WINDOW", set_destroy)
+
+    s = "You ran out of tries.\nThe correct sequence was:"
+    tk.Label(pop, text=s).pack()
+    for i in range(4):
+        CustomCircle(pop, seq[i]).pack(side=LEFT)
+    tk.Button(pop, text="Try Again", padx = 10, command=set_destroy).pack()
+    tk.Button(pop, text="Exit", padx = 10, command=set_kill).pack()
+    pop.resizable(0, 0)
+
+    while(not destroy and not kill):
+        pop.update()
+
+# Code Marathon
 def play():
     #########################
     # Set Up Colour Buttons #
@@ -188,18 +241,19 @@ def play():
     mm.title("Master Mind")
     mm.geometry("334x720")
     mm.resizable(0, 0)
+    mm.protocol("WM_DELETE_WINDOW", set_kill)
     b0 = PegBoard(mm, 'black', 0)
-    b0.grid(row=13,column=0)
+    b0.grid(row=0,column=0)
     b1 = CustomButton(mm)
-    b1.grid(row=13,column=1)
+    b1.grid(row=0,column=1)
     b2 = CustomButton(mm)
-    b2.grid(row=13,column=2)
+    b2.grid(row=0,column=2)
     b3 = CustomButton(mm)
-    b3.grid(row=13,column=3)
+    b3.grid(row=0,column=3)
     b4 = CustomButton(mm)
-    b4.grid(row=13,column=4)
+    b4.grid(row=0,column=4)
     enter = EnterButton(mm)
-    enter.grid(row=13,column=5)
+    enter.grid(row=0,column=5)
     mm.update()
 
     ###############
@@ -209,7 +263,7 @@ def play():
     menu = Menu(menubar, tearoff=0)
     menu.add_command(label="Colours", command=colour_menu)
     menu.add_command(label="Difficulty", command=difficulty_menu)
-    menu.add_command(label="Restart", command=colour_menu)
+    menu.add_command(label="Restart", command=set_destroy)
     menu.add_separator()
     menu.add_command(label="Instructions", command=instructions)
     menu.add_command(label="About", command=colour_menu)
@@ -222,29 +276,40 @@ def play():
     # Begin Playing The Game #
     ##########################
     generate_sequence()
-    tries = 12
+    print(seq)
+    global diff
+    tries = 8 if diff==2 else 12
     while tries>0:
+        global destroy
+        destroy = False
         mm.update()
+        if destroy:
+            mm.destroy()
+            play()
+        if kill:
+            mm.destroy()
+            exit()
         if(enter.press=='true'):
             colours = [b1.colour, b2.colour, b3.colour, b4.colour]
             for i in range(4):
                 bx = CustomCircle(mm, colours[i])
-                bx.grid(row=12-tries, column=i+1)
+                bx.grid(row=13-tries, column=i+1)
             enter.press = 'false'
             bw = check_pegs(colours)
             if bw[0] == 4:
-                print("Congratulations! You found the sequence")
-                #restart
+                vic_pop(13-tries)
             pbw = PegBoard(mm, 'white', bw[1])
             pbb = PegBoard(mm, 'black', bw[0])
-            pbw.grid(row=12-tries, column=5)
-            pbb.grid(row=12-tries, column=0)
+            pbw.grid(row=13-tries, column=5)
+            pbb.grid(row=13-tries, column=0)
             tries -= 1
             if tries == 0:
-                print("Sorry, you lost")
-                for i in range(4):
-                    answ = CustomCircle(mm, seq[i])
-                    answ.grid(row=0, column=i+1)
-                mm.mainloop()
+                loss_pop()
+    if destroy:
+        mm.destroy()
+        play()
+    if kill:
+        mm.destroy()
+        exit()
 
 play()
